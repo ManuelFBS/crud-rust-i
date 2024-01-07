756fec8c5115f78146ed526731ecb51324f7eb67
ManuelFBS <manuelf.borrego@gmail.com>
2024-01-07 00:08:29 -0400

    Modificación de user.rs, Creación de repository.rs

--- src/user.rs
+++ src/user.rs
@@ -1,14 +1,14 @@
 use chrono::{DateTime, NaiveDate, Utc};
 
 pub struct User {
-    id: uuid::Uuid,
-    name: String,
-    birth_date: NaiveDate,
-    custom_data: CustomData,
-    created_at: Option<DateTime<Utc>>,
-    updated_at: Option<DateTime<Utc>>,
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub birth_date: NaiveDate,
+    pub custom_data: CustomData,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
 }
 
 pub struct CustomData {
-    random: u32,
+    pub random: u32,
 }

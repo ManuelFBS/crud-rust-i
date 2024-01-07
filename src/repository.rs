bd6ab378bb9016d811cec620392e17351047ca87
ManuelFBS <manuelf.borrego@gmail.com>
2024-01-07 00:36:25 -0400

    Actualización de repository.rs

--- src/repository.rs
+++ src/repository.rs
@@ -1,7 +1,17 @@
+use crate::user::User;
+
 pub trait Repository {
     fn get_users(&self, user_id: uuid::Uuid) -> Result<User, String>;
 }
 
 pub struct MemoryRepository {
     user: Vec<User>,
+}
+
+impl Default for MemoryRepository {
+    fn default() -> Self {
+        Self {
+            users: vec![User::new("Manuel", (1962, 03, 04))],
+        }
+    }
 }

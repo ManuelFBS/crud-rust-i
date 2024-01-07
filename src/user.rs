8e0bda3b2d915e84134186b78c01f66328eccda6
ManuelFBS <manuelf.borrego@gmail.com>
2024-01-07 00:04:33 -0400

    Actualización de user.rs

--- src/user.rs
+++ src/user.rs
@@ -1,8 +1,12 @@
+use chrono::{DateTime, NaiveDate, Utc};
+
 pub struct User {
     id: uuid::Uuid,
     name: String,
     birth_date: NaiveDate,
     custom_data: CustomData,
+    created_at: Option<DateTime<Utc>>,
+    updated_at: Option<DateTime<Utc>>,
 }
 
 pub struct CustomData {

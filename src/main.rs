df94ac7afc6e64c6053db4a773baf26987f8c14d
ManuelFBS <manuelf.borrego@gmail.com>
2024-01-06 23:26:50 -0400

    Actualización de main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,25 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use core::sync::atomic::AtomicU16;
-use core::sync::atomic::Ordering;
-use std::sync::Arc;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {} !!!", &name)
 }
 
-async fn health_check(thread_index: u16) -> HttpResponse {
-    HttpResponse::Ok()
-        .append_header(("thread-id", thread_index.to_string()))
-        .finish()
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // dotenv::dotenv().ok();
-    // let port = std::env::var("PORT").unwrap_or("8085".to_string());
+    dotenv::dotenv().ok();
+    let port = std::env::var("PORT").unwrap_or("8085".to_string());
+    let address = format!("127.0.0.1:{}", port);
 
     println!("Starting the server...");
-    let thread_counter = Arc::new(AtomicU16::new(1));
-    HttpServer::new(move || {
-        println!(
-            "Starting the thread: {}",
-            thread_counter.fetch_add(1, Ordering::SeqCst)
-        );
-        let thread_index = thread_counter.load(Ordering::SeqCst);
+    HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
-            .route("/health", web::get().to(move || health_check(thread_index)))
+            .route("/health", web::get().to(|| HttpResponse::Ok()))
             .route("/str", web::get().to(|| async { "Hello Rust" }))
             .route("/{name}", web::get().to(greet))
     })
-    .bind(("localhost", 8085))?
+    .bind(&address)?
     .run()
     .await
 }
